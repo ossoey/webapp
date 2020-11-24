8ccc4b2c2f7fb51ee342b7d6526aeefedecc3481
Ebanga jj <ebanga@hotmail.com>
2020-11-23 19:57:28 -0500

    installation de ejs et Ajout d'un fichier test ejs

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
-
+app.set('view engine', 'ejs')
 
 let rootObj = { root : __dirname};
 
 app.get('/', (req, res) => {
- // res.send('Hello Ebanga World!');
   res.sendFile('public/index.html' , rootObj );
 });
 
 app.get('/dynipage', (req, res) => {
-  // res.send('Hello Ebanga World!');
   res.sendFile('public/dynipage.html' , rootObj );
 });
 
+// app.get('/profile/:name', (req, res) => {
+//   // res.send('Hello Ebanga World!');
+//
+//   res.sendFile('public/dynipage.html' , rootObj );
+// });
 
-app.get('/coco1', (req, res) => {
-  //res.sendFile("/exemple.txt");
-  res.sendFile('public/exemple.txt' , rootObj );
+app.get('/profile/:name', (req, res) => {
+  res.render('profile',{person:req.params.name});
 });
-
-app.get('/ab?cd', function (req, res) {
-
-  res.sendFile('public/cite/coco.txt' , { root : __dirname});
-})
 
 
 app.listen(port, () => {
