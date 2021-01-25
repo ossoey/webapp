c6648a99dad8835e783d375b79db840243269b21
Ebanga jj <ebanga@hotmail.com>
2021-01-25 08:53:18 -0500

    Ajouts de 4 pays préfixé par Ba, Implémentation des index de données(Continent, pays, capitale). Pour chaque valeur donnée, renvoyé sa position dans son ensemble de données. Tests

--- public/quizzexp/javascripts/quizzexp.js
+++ public/quizzexp/javascripts/quizzexp.js
@@ -1,5 +1,7 @@
 
-Ebika.Projects.Quizzexp   = class EbikaProjectsQuizzexp     extends Ebika   {
+const   continents = ["Afrique", "Amérique", "Asie", "Europe", "Océanie"];
+
+Ebika.Projects.Quizzexp   = class EbikaProjectsQuißzzexp     extends Ebika   {
     constructor(paramsIn) {
         let turn  =  0;
         super();
@@ -8,20 +10,24 @@ Ebika.Projects.Quizzexp   = class EbikaProjectsQuizzexp     extends Ebika   {
 
 
                 "references" :  [
-                    {"pays": "Afghanistan", "capitale": "Kaboul",  "continent" : "Asie", "preposition":"de l'"} ,
-                    {"pays": "Afrique du Sud", "capitale": "Pretoria",  "continent" : "Afrique", "preposition":"de l'"} ,
-                    {"pays": "Albanie", "capitale": "Tirana",  "continent" : "Europe", "preposition":"de l'"} ,
-                    {"pays": "Algérie", "capitale": "Alger",  "continent" : "Afrique", "preposition":"de l'"} ,
-                    {"pays": "Allemagne", "capitale": "Berlin",  "continent" : "Europe", "preposition":"de l'"} ,
-                    {"pays": "Andorre", "capitale": "Andorre-la-vielle",  "continent" : "Europe", "preposition":"de l'"} ,
-                    {"pays": "Angola", "capitale": "Luanda",  "continent" : "Afrique", "preposition":"de l'"} ,
-                    {"pays": "Antigua-et-Barbuda", "capitale": "Saint John’s",  "continent" : "Amérique", "preposition":"de l'"} ,
-                    {"pays": "Arabie saoudite", "capitale": "Ryad",  "continent" : "Asie", "preposition":"de l'"} ,
-                    {"pays": "Argentine", "capitale": "Buenos Aires",  "continent" : "Amérique", "preposition":"de l'"} ,
-                    {"pays": "Arménie", "capitale": "Erevan",  "continent" : "Asie", "preposition":"de l'"} ,
-                    {"pays": "Australie", "capitale": "Canberra",  "continent" : "Oceanie", "preposition":"de l'"} ,
-                    {"pays": "Austriche", "capitale": "Vienne",  "continent" : "Europe", "preposition":"de l'"} ,
-                    {"pays": "Azerbaïdjan", "capitale": "Bakou",  "continent" : "Europe", "preposition":"de l'"} ,
+                    {"pays": "Afghanistan", "capitale": "Kaboul",  "continent" :  continents [2], "preposition":"de l'",  } ,
+                    {"pays": "Afrique du Sud", "capitale": "Pretoria",  "continent" :  continents [0], "preposition":"de l'"} ,
+                    {"pays": "Albanie", "capitale": "Tirana",  "continent" : continents [3], "preposition":"de l'"} ,
+                    {"pays": "Algérie", "capitale": "Alger",  "continent" : continents [0], "preposition":"de l'"} ,
+                    {"pays": "Allemagne", "capitale": "Berlin",  "continent" :  continents [3], "preposition":"de l'"} ,
+                    {"pays": "Andorre", "capitale": "Andorre-la-vielle",  "continent" :  continents [3], "preposition":"de l'"} ,
+                    {"pays": "Angola", "capitale": "Luanda",  "continent" : continents [0], "preposition":"de l'"} ,
+                    {"pays": "Antigua-et-Barbuda", "capitale": "Saint John’s",  "continent" : continents [1], "preposition":"de l'"} ,
+                    {"pays": "Arabie saoudite", "capitale": "Ryad",  "continent" : continents [2], "preposition":"de l'"} ,
+                    {"pays": "Argentine", "capitale": "Buenos Aires",  "continent" :  continents [1], "preposition":"de l'"} ,
+                    {"pays": "Arménie", "capitale": "Erevan",  "continent" :  continents [2], "preposition":"de l'"} ,
+                    {"pays": "Australie", "capitale": "Canberra",  "continent" : continents [5], "preposition":"de l'"} ,
+                    {"pays": "Austriche", "capitale": "Vienne",  "continent" : continents [3], "preposition":"de l'"} ,
+                    {"pays": "Azerbaïdjan", "capitale": "Bakou",  "continent" :  continents [3], "preposition":"de l'"} ,
+                    {"pays": "Bahamas", "capitale": "Nasseau",  "continent" :  continents [1], "preposition":"de l'"} ,
+                    {"pays": "Bahreîn", "capitale": "Manama",  "continent" :  continents [2], "preposition":"de l'"} ,
+                    {"pays": "Bangladesh", "capitale": "Dacca",  "continent" :  continents [2], "preposition":"de l'"} ,
+                    {"pays": "Barbade", "capitale": "BridgeTown",  "continent" :  continents [1], "preposition":"de l'"} ,
                    ],
                 "questions" : [
                     {"question" : "Quelle est la capitale $0$ ?","qindex": "pays", "rindex":"capitale","propindex":"preposition"  },
@@ -30,6 +36,54 @@ Ebika.Projects.Quizzexp   = class EbikaProjectsQuizzexp     extends Ebika   {
                     {"question" : " $, est dans quel continent? :","qindex": "capitale", "rindex":"continent"},
                 ],
 
+                indexOfContinent : function(continent) {
+                    return continents.indexOf(continent);
+                }   ,
+
+                indexOfCountry : function(country) {
+                    let indexOf = -1, trouver = false;
+
+                    let countryIndex = 0;
+
+                    while((countryIndex<this.references.length)&&(!trouver)) {
+                        let pays = this.references[countryIndex]["pays"];
+                        if( pays===country) {
+                            indexOf = countryIndex;
+                            trouver = true;
+                        }
+                        countryIndex++;
+                    };
+
+                    return indexOf;
+                }   ,
+
+                indexOfCapital: function(capitalIn) {
+                    let indexOf = -1, trouver = false;
+
+                    let capitalIndex = 0;
+
+                    while((capitalIndex<this.references.length)&&(!trouver)) {
+                        let capital =  this.references[capitalIndex]["capitale"];
+                        if(   capital ===capitalIn) {
+                            indexOf = capitalIndex;
+                            trouver = true;
+                        }
+                        capitalIndex++;
+                    };
+
+                    return indexOf;
+                }   ,
+
+                tests : function () {
+
+                    let test  =[ [["Amérique" ,this.indexOfContinent("Amérique")], ["Europe" ,this.indexOfContinent("Europe")],  ["Europew" ,this.indexOfContinent("Europew")]],
+                                  [["Bahamas" ,this.indexOfCountry("Bahamas")], ["Andorre" ,this.indexOfCountry("Andorre")], ["Andorre2" ,this.indexOfCountry("Andorre2")]],
+                                   [["Vienne" ,this.indexOfCapital("Vienne")], ["Alger" ,this.indexOfCapital("Alger")], ["Andorre2" ,this.indexOfCapital("Andorre2")]],
+                        ]
+                    console.log(test);
+                   return  test;
+
+                }  ,
 
                 buildGivenQuestion : function (targetReferenceIndex,targetQuestionIndex) {
                     let str          =  this.questions[targetQuestionIndex]["question"]
@@ -58,10 +112,12 @@ Ebika.Projects.Quizzexp   = class EbikaProjectsQuizzexp     extends Ebika   {
 
         let infos = [];
 
-        for (let paysIndex = 0 ;paysIndex< this.quizzes.wordcapitals.references.length ;paysIndex ++ ) {
-            infos.push(this.quizzes.wordcapitals.buildGivenQuestion(paysIndex,0));
-        };
-        return infos;
+        // for (let paysIndex = 0 ;paysIndex< this.quizzes.wordcapitals.references.length ;paysIndex ++ ) {
+        //     infos.push(this.quizzes.wordcapitals.buildGivenQuestion(paysIndex,0));
+        // };
+
+        this.quizzes.wordcapitals.tests();
+        //return infos;
 
     };
 
@@ -91,5 +147,6 @@ Ebika.Projects.Quizzexp   = class EbikaProjectsQuizzexp     extends Ebika   {
 
 const  quizz  = new Ebika.Projects.Quizzexp();
 function main() {
+    quizz.essay();
    // quizz.doTests();
 }
